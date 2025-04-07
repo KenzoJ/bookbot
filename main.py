16df1ed4091037f7f303cbb0a05ce641306d9504
KenzoJ <charles.felux@gmail.com>
2025-04-07 17:32:12 -0400

    add function to count all characters and sort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_text, count_words, list_characters, make_dict_from_list
+from stats import get_book_text, count_words, list_characters, make_dict_from_list, sort_dict, make_pretty_count
 
 def main():
     path = "./books/frankenstein.txt"
@@ -6,6 +6,10 @@ def main():
     new_list = list_characters(book_text)
     dict_of_characters = make_dict_from_list(new_list)
     # print(f'{count_words(book_text)} words found in the document')
-    print(dict_of_characters)
+    sorted_dict = sort_dict(dict_of_characters)
+    print(f'============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {count_words(book_text)} total words\n--------- Character Count -------')
+    final_list = make_pretty_count(sorted_dict)
+    for i in range(len(final_list)):
+        print(final_list[i])
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,10 @@
-    
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
 def count_words(book_text):
-    return len(book_text.split().lower())
+    return len(book_text.split())
 
 def list_characters(book_text):
     return list(book_text)
@@ -18,4 +17,25 @@ def make_dict_from_list(new_list):
             dict_of_characters[c] +=  1 
         else: dict_of_characters[c] = 1
     return dict_of_characters
-   
+  
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dict(ex_list):
+    temp_list = []
+    for k, v in ex_list.items():
+        if k.isalpha():
+            temp_d = {"name": k, "num": v}
+            temp_list.append(temp_d)
+
+    temp_list.sort(reverse=True, key=sort_on)
+    return temp_list
+
+def make_pretty_count(l):
+    final_list = []
+    for i in range(len(l)):
+        formatted_string = f'{l[i]["name"]}: {l[i]["num"]}'
+        final_list.append(formatted_string)
+    return final_list
+
+
