1f4be8cd72a74eea09179ca0c71073a72de83fa6
KenzoJ <charles.felux@gmail.com>
2025-04-07 14:06:08 -0400

    create main to print single book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    path = "./books/frankenstein.txt"
+    print(get_book_text(path))
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+main()
