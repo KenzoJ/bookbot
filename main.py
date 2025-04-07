4f5b6ce1da6e7c0a55d6f6f014ef7c45e46441e8
KenzoJ <charles.felux@gmail.com>
2025-04-07 15:02:56 -0400

    add ability to count all characters

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from stats import get_book_text, count_words
+from stats import get_book_text, count_words, list_characters, make_dict_from_list
 
 def main():
     path = "./books/frankenstein.txt"
     book_text = get_book_text(path)
-    print(f'{count_words(book_text)} words found in the document')
+    new_list = list_characters(book_text)
+    dict_of_characters = make_dict_from_list(new_list)
+    # print(f'{count_words(book_text)} words found in the document')
+    print(dict_of_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,21 @@
-
+    
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
 def count_words(book_text):
-    return len(book_text.split())
+    return len(book_text.split().lower())
+
+def list_characters(book_text):
+    return list(book_text)
+
+def make_dict_from_list(new_list):
+    dict_of_characters = {}
+    for c in new_list:
+        c = c.lower()
+        if c in dict_of_characters:
+            dict_of_characters[c] +=  1 
+        else: dict_of_characters[c] = 1
+    return dict_of_characters
+   
